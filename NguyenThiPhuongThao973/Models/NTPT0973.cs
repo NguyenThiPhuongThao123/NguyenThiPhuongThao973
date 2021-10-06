1c6b890cb9ee5bcd24397527f60a5426b3bb40af
Nguyen Thi Phuong Thao <phuongthaoov0408@gmail.com>
2021-10-06 07:51:52 +0700

    Create_Table_Person_NTPT973

--- NguyenThiPhuongThao973/Models/NTPT0973.cs
+++ NguyenThiPhuongThao973/Models/NTPT0973.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +11,10 @@ namespace NguyenThiPhuongThao973.Models
     {
         [Key]
         public int NTPTId { get; set; }
+        [Column(TypeName = "varchar(20)")]
         public string NTPTName { get; set; }
+        [Column(TypeName = "varchar(50)")]
         public string NTPTGender { get; set; }
+
     }
 }
